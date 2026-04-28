b90ea8c5aefb33e71e2601c3412a79407d0c2ae8
1010Seki <117787546+LeaF127@users.noreply.github.com>
2026-04-28 10:48:45 +0800

    update providers/doubao/__init__.py

--- providers/doubao/__init__.py
+++ providers/doubao/__init__.py
@@ -1,4 +1,5 @@
 from providers.doubao.asr import DoubaoASR
+from providers.doubao.asr_bigmodel import DoubaoBigModelASR
 from providers.doubao.tts import DoubaoTTS
 
-__all__ = ["DoubaoASR", "DoubaoTTS"]
+__all__ = ["DoubaoASR", "DoubaoBigModelASR", "DoubaoTTS"]
